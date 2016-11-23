cc2786f95680a6a02d988940e74c1c5fdc8090f1
Leo Kaliazine/Josh Allen <lkaliazine@connectedlab.com>
2016-11-23 10:11:25 -0500

    [#134781127] Fixed project structure

--- Example/MotionExample/index.android.js
+++ Example/MotionExample/index.android.js
@@ -20,11 +20,11 @@ export default class MotionExample extends Component {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          To get started, edit index.android.js
         </Text>
         <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
         </Text>
       </View>
     );
